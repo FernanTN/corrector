7faa9efac05074c9cdc9b45f4397d75cd739bcd6
Fernando Torrado Navarro <torradof@gmail.com>
2016-03-04 23:07:19 +0100

    Más procedimientos

--- src/pparaula.adb
+++ src/pparaula.adb
@@ -64,5 +64,31 @@ package body pparaula is
       end loop;
       return paraula;
    end toString;
+   -- Procediment per a copiar una paraula
+   procedure copiar(desti : out tparaula; origen : in tparaula) is
+      i: tllargaria;
+   begin
+      i:=1;
+      desti.llargaria := origen.llargaria;
+      for i in 1 .. origen.llargaria loop
+         desti.lletres(i) := origen.lletres(i);
+      end loop;
+   end copiar;
+   -- Procediment per tractar amb les paraules llegides del teclat
+   procedure open(origen : out OrigenParaules) is
+   begin
+   end open;
+   -- Procediment per tractar amb les paraules llegides del fitxer nom
+   procedure open(origen : out OrigenParaules; nom : in String) is
+   begin
+   end open;
+   -- Procediment per tancar l'origen de les paraules
+   procedure close(origen : in out OrigenParaules) is
+   begin
+   end close;
+   -- Procediment per llegir una paraula des d'un origen de paraules
+   procedure get(origen : in out OrigenParaules; p: out tparaula) is
+   begin
+   end get;
 
 end pparaula;
